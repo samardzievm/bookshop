1eb685976467676ae2e6e4d805c3df3490a2be8c
Мартин <martin.samardziev@students.finki.ukim.mk>
2021-02-15 22:13:16 +0100

    Added Books controller and views

---
+++ bookshop/Migrations/20210215203200_AddShortDescriptionToBook.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bookshop.Migrations
+{
+    public partial class AddShortDescriptionToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShortDescription",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortDescription",
+                table: "Book");
+        }
+    }
+}

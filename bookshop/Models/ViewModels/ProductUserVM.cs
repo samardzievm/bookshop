6dd74256186b695c37744e1996851d9fff18956b
Мартин <martin.samardziev@students.finki.ukim.mk>
2021-02-17 19:49:18 +0100

    Added Summary product list, confirmation message, UI changes

---
+++ bookshop/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookshop.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        // this VM will display the User details and Product details
+        public ProductUserVM()
+        {
+            ProductList = new List<Book>();
+        }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public IList<Book> ProductList { get; set; } // will be approached with for loop instead of foreach. foreach works for IEnuerable
+    }
+}
